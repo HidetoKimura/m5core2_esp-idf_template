4e88e4f22c69789b208ebd7c86da9d5b1afbddaa
HidetoKimura <hideto.kimura.apple@gmail.com>
2022-10-09 08:28:11 +0900

    update.

--- main/main.c
+++ main/main.c
@@ -21,34 +21,7 @@
 
 #define LV_TICK_PERIOD_MS 1
 
-
-enum toggle_id {
-	TOGGLE_LED = 0,
-	TOGGLE_VIB,
-	TOGGLE_5V,
-};
-
-static void toggle_event_cb(lv_obj_t *toggle, lv_event_t event)
-{
-	if(event == LV_EVENT_VALUE_CHANGED) {
-		bool state = lv_switch_get_state(toggle);
-		enum toggle_id *id = lv_obj_get_user_data(toggle);
-
-		// Note: This is running in the GUI thread, so prolonged i2c
-		// comms might cause some jank
-		switch (*id) {
-		case TOGGLE_LED:
-			m5core2_led(state);
-			break;
-		case TOGGLE_VIB:
-			m5core2_vibration(state);
-			break;
-		case TOGGLE_5V:
-			m5core2_int_5v(state);
-			break;
-		}
-	}
-}
+static uint64_t current_tick = 0;
 
 static void gui_timer_tick(void *arg)
 {
@@ -56,33 +29,72 @@ static void gui_timer_tick(void *arg)
 	(void) arg;
 
 	lv_tick_inc(LV_TICK_PERIOD_MS);
+
+	current_tick++;
+
+}
+
+static void disp_init(void)
+{
+	static lv_color_t bufs[2][DISP_BUF_SIZE];
+	static lv_disp_draw_buf_t  disp_buf;
+	uint32_t size_in_px = DISP_BUF_SIZE;
+
+	// Set up the frame buffers
+	lv_disp_draw_buf_init(&disp_buf, &bufs[0], &bufs[1], size_in_px);
+
+	// Set up the display driver
+	static lv_disp_drv_t disp_drv;
+	lv_disp_drv_init(&disp_drv);
+	disp_drv.flush_cb = disp_driver_flush;
+	disp_drv.draw_buf = &disp_buf;
+    disp_drv.hor_res = LV_HOR_RES_MAX;
+    disp_drv.ver_res = LV_VER_RES_MAX;
+	lv_disp_drv_register(&disp_drv);
+
+	// Register the touch screen. All of the properties of it
+	// are set via the build config
+	static lv_indev_drv_t indev_drv;
+	lv_indev_drv_init(&indev_drv);
+	indev_drv.read_cb = touch_driver_read;
+	indev_drv.type = LV_INDEV_TYPE_POINTER;
+	lv_indev_drv_register(&indev_drv);
+}
+
+#define GUI_WID_BODY_ROW_MAX 	14
+
+typedef struct {
+	lv_obj_t* title_label;
+	lv_obj_t* body_label[GUI_WID_BODY_ROW_MAX];
+} gui_wid_obj_t;
+
+static gui_wid_obj_t gui_wid;
+
+static void widgets_create(void)
+{
+    gui_wid.title_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(gui_wid.title_label, "Hello World");
+	lv_obj_align(gui_wid.title_label, LV_ALIGN_TOP_LEFT, 0, 0);
+
+	for (int i = 0; i < GUI_WID_BODY_ROW_MAX; i++) 
+	{
+		gui_wid.body_label[i] = lv_label_create(lv_scr_act());
+		lv_label_set_text_fmt(gui_wid.body_label[i],"%d:", i);
+		lv_obj_align(gui_wid.body_label[i], LV_ALIGN_TOP_LEFT, 0, 16 * (i + 1));
+	}
+
+}
+
+static void widgets_update(void)
+{
+	lv_label_set_text_fmt(gui_wid.body_label[GUI_WID_BODY_ROW_MAX - 1], "Current Tick: %lld", current_tick);
 }
 
 static void gui_thread(void *pvParameter)
 {
 	(void) pvParameter;
 
-	static lv_color_t bufs[2][DISP_BUF_SIZE];
-	static lv_disp_buf_t disp_buf;
-	uint32_t size_in_px = DISP_BUF_SIZE;
-
-	// Set up the frame buffers
-	lv_disp_buf_init(&disp_buf, &bufs[0], &bufs[1], size_in_px);
-
-	// Set up the display driver
-	lv_disp_drv_t disp_drv;
-	lv_disp_drv_init(&disp_drv);
-	disp_drv.flush_cb = disp_driver_flush;
-	disp_drv.buffer = &disp_buf;
-	lv_disp_drv_register(&disp_drv);
-
-	// Register the touch screen. All of the properties of it
-	// are set via the build config
-	lv_indev_drv_t indev_drv;
-	lv_indev_drv_init(&indev_drv);
-	indev_drv.read_cb = touch_driver_read;
-	indev_drv.type = LV_INDEV_TYPE_POINTER;
-	lv_indev_drv_register(&indev_drv);
+	disp_init();
 
 	// Timer to drive the main lvgl tick
 	const esp_timer_create_args_t periodic_timer_args = {
@@ -93,46 +105,12 @@ static void gui_thread(void *pvParameter)
 	ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
 	ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-
-	// Full screen root container
-	lv_obj_t *root = lv_cont_create(lv_scr_act(), NULL);
-	lv_obj_set_size(root, 320, 240);
-	lv_cont_set_layout(root, LV_LAYOUT_COLUMN_MID);
-	// Don't let the containers be clicked on
-	lv_obj_set_click(root, false);
-
-	// Create rows of switches for different functions
-	struct {
-		const char *label;
-		bool init;
-		enum toggle_id id;
-	} switches[] = {
-		{ "LED",     true,  TOGGLE_LED },
-		{ "Vibrate", false, TOGGLE_VIB },
-		{ "5V Bus",  false, TOGGLE_5V },
-	};
-	for (int i = 0; i < sizeof(switches) / sizeof(switches[0]); i++) {
-		lv_obj_t *row = lv_cont_create(root, NULL);
-		lv_cont_set_layout(row, LV_LAYOUT_ROW_MID);
-		lv_obj_set_size(row, 200, 0);
-		lv_cont_set_fit2(row, LV_FIT_NONE, LV_FIT_TIGHT);
-		// Don't let the containers be clicked on
-		lv_obj_set_click(row, false);
-
-		lv_obj_t *toggle = lv_switch_create(row, NULL);
-		if (switches[i].init) {
-			lv_switch_on(toggle, LV_ANIM_OFF);
-		}
-		lv_obj_set_user_data(toggle, &switches[i].id);
-		lv_obj_set_event_cb(toggle, toggle_event_cb);
-
-		lv_obj_t *label = lv_label_create(row, NULL);
-		lv_label_set_text(label, switches[i].label);
-	}
+	widgets_create();
 
 	while (1) {
 		vTaskDelay(10 / portTICK_PERIOD_MS);
 
+		widgets_update();
 		lv_task_handler();
 	}
 
@@ -142,8 +120,6 @@ static void gui_thread(void *pvParameter)
 
 void app_main(void)
 {
-	printf("Hello world!\n");
-
 	/* Print chip information */
 	esp_chip_info_t chip_info;
 	esp_chip_info(&chip_info);
